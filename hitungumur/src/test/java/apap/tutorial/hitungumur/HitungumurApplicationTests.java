988dfd70b9413a303efe3c818931cb208cacd7ad
nuga99 <nugamuhammad@gmail.com>
2020-01-26 17:25:12 +0700

    feat: tutorial-1-hitungumur

---
+++ hitungumur/src/test/java/apap/tutorial/hitungumur/HitungumurApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tutorial.hitungumur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitungumurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
